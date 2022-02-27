d1813f8230bc70c846e8e4b34eb22ab33282dadd
꧁Dulen sathsara꧂ <99535426+dulensathsara@users.noreply.github.com>
2022-02-27 18:13:51 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+class Config:
+    API_ID = "10678770"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "ba2e779957affb18931487bf2dfaa189" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5169411964:AAEtbZapfGKBVkgIo-vvCGIAtiffvm9yiNw" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+© 2022 GitHub, Inc.
+Terms
+Privacy
